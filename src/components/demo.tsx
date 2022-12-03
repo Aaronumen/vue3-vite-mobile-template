51b123097c64fcc810013d66bfdd0682578a7932
sunyi <1076552287@qq.com>
2022-12-03 16:16:52 +0800

    ✅ test: \<\>\</\> tsx测试

--- src/components/demo.tsx
+++ src/components/demo.tsx
@@ -2,8 +2,8 @@ export default defineComponent({
   setup() {
     const demo = ref("demo page")
     const test=ref('test')
-    return () => <div>{demo.value}
+    return () => <>{demo.value}
     <div>{test.value}</div>
-    </div>
+    </>
   }
 })
